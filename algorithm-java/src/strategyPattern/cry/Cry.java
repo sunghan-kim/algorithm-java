26b2106748363b663d7e2c26805d9302022980be
mktinfo <mktinfo@mktinfo-PC>
2019-09-18 16:32:20 +0900

    encapsulation - ..ing

---
+++ algorithm-java/src/strategyPattern/cry/Cry.java
@@ -0,0 +1,7 @@
+package strategyPattern.cry;
+
+public interface Cry {
+
+	public void cry();
+	
+}
---
+++ algorithm-java/src/strategyPattern/fly/Fly.java
@@ -0,0 +1,7 @@
+package strategyPattern.fly;
+
+public interface Fly {
+
+	public void fly();
+	
+}
