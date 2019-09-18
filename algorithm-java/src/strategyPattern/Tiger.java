eba6898e5e729c6408ef9b657cb3b994ab27fe7c
shkim4738 <shkim4738@gmail.com>
2019-09-18 19:54:52 +0900

    Strategy Pattern - encapsulation Finished

--- algorithm-java/src/strategyPattern/Tiger.java
+++ algorithm-java/src/strategyPattern/Tiger.java
@@ -1,18 +1,29 @@
 package strategyPattern;
 
+import strategyPattern.cry.TigerCry;
+import strategyPattern.fly.FlyNoway;
+
 //https://hyeonstorage.tistory.com/146?category=549763
 
-public class Tiger extends Animal {
+public class Tiger extends Animal { // Animal 상속
 
-	public Tiger() {
+	public Tiger() { // Tiger 객체 생성자
+		
+		cry = new TigerCry();	// Animal의 cry 변수에 Cry 인터페이스의 TigerCry를 연결한다.	
+		fly = new FlyNoway();	// Animal의 fly 변수에 Fly 인터페이스의 FlyNoway를 연결한다. 
 		
 	}
 	
-	public void cry() {
-		System.out.println("어흥");
-	}
+	/*
+	 * public void cry() { System.out.println("어흥"); }
+	 * 
+	 * public void display() { System.out.println("호랑이"); }
+	 */
 	
+	
+	@Override
 	public void display() {
+		// TODO Auto-generated method stub
 		System.out.println("호랑이");
 	}
 	
---
+++ algorithm-java/src/strategyPattern/fly/FlyWithWings.java
@@ -0,0 +1,11 @@
+package strategyPattern.fly;
+
+public class FlyWithWings implements Fly {
+	
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		System.out.println("날개로 날다");
+	}
+
+}
