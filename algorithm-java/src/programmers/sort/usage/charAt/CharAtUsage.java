f1d90550ee36e8fcf46484184c8f932beb69c4ad
sunghan-kim <shkim4738@gmail.com>
2019-09-26 17:18:10 +0900

    Usage of CharAt

---
+++ algorithm-java/src/programmers/sort/usage/charAt/CharAtUsage.java
@@ -0,0 +1,24 @@
+package programmers.sort.usage.charAt;
+
+// https://hunit.tistory.com/165
+
+/**
+ * 
+ * @author shkim
+ * 
+ * # charAt
+ * 
+ *   - 문장 중에 인덱스 위치에 해당되는 문자 추출
+ *
+ */
+
+public class CharAtUsage {
+
+	public static void main(String[] args) {
+		
+		String aaa = "가나다라마바사";
+		System.out.println(aaa.charAt(3));
+		
+	}
+	
+}
