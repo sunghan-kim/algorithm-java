b6be6de581d2a994dce632f69d076191d97c03df
shkim4738 <shkim4738@gmail.com>
2020-09-06 15:39:08 +0900

    백준 - 정렬 알고리즘(기본) - 수 정렬하기

---
+++ algorithm-java/src/boj/sortAlgorithm/basic/numberSorting/sol1/Main.java
@@ -0,0 +1,32 @@
+package boj.sortAlgorithm.basic.numberSorting.sol1;
+
+/*
+ *   문제명 : 수 정렬하기
+ *      - https://www.acmicpc.net/problem/2750
+ *      - 성공
+ * */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i=0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int a : arr) {
+            sb.append(a).append("\n");
+        }
+        br.close();
+        System.out.println(sb.toString());
+    }
+}
