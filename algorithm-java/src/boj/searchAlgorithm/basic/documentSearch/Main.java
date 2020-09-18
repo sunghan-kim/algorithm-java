2928c50153e8880d1d105de0b2bdb58f7fa27eeb
shkim4738 <shkim4738@gmail.com>
2020-09-18 23:30:00 +0900

    백준 - 탐색 알고리즘(기본) - 문서 찾기 (진행중)

--- algorithm-java/src/boj/searchAlgorithm/basic/documentSearch/Main.java
+++ algorithm-java/src/boj/searchAlgorithm/basic/documentSearch/Main.java
@@ -6,5 +6,18 @@ package boj.searchAlgorithm.basic.documentSearch;
  *   - https://www.acmicpc.net/problem/1543
  * */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String document = br.readLine();
+        String word = br.readLine();
+
+
+
+        br.close();
+    }
 }
