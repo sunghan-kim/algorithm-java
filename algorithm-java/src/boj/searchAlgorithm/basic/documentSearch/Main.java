834d0bde131440717c42943f2ea56c7e3a9d669c
shkim4738 <shkim4738@gmail.com>
2020-09-20 23:23:38 +0900

    백준 - 탐색 알고리즘(기본) - 문서 찾기

--- algorithm-java/src/boj/searchAlgorithm/basic/documentSearch/Main.java
+++ algorithm-java/src/boj/searchAlgorithm/basic/documentSearch/Main.java
@@ -4,6 +4,7 @@ package boj.searchAlgorithm.basic.documentSearch;
  *   문제명 : 문서 찾기
  *
  *   - https://www.acmicpc.net/problem/1543
+ *   - 성공
  * */
 
 import java.io.BufferedReader;
@@ -15,12 +16,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String document = br.readLine();
         String word = br.readLine();
-
+        int wordLen = word.length();
+        int docLen = document.length();
+        int result = 0;
         int idx = 0;
 
-        while (idx < document.length()) {
-
+        while (idx < docLen) {
+            int lastIdx = Math.min(idx + wordLen, docLen);
+            if (word.equals(document.substring(idx, lastIdx))) {
+                result++;
+                idx += wordLen;
+            } else {
+                idx++;
+            }
         }
+
+        System.out.println(result);
 
         br.close();
     }
