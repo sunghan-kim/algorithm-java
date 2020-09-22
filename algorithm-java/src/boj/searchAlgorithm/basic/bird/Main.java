a4be127b892ebd1152e54599815e20be6ad010b2
shkim4738 <shkim4738@gmail.com>
2020-09-22 23:19:54 +0900

    백준 - 탐색 알고리즘(기본) - 새 (진행중)

--- algorithm-java/src/boj/searchAlgorithm/basic/bird/Main.java
+++ algorithm-java/src/boj/searchAlgorithm/basic/bird/Main.java
@@ -6,5 +6,10 @@ package boj.searchAlgorithm.basic.bird;
  *   - https://www.acmicpc.net/problem/1568
  * */
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
