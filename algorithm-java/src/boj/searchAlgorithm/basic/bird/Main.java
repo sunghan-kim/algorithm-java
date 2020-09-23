2738c76192bc71a671621d93bfc5e81216d3691c
shkim4738 <shkim4738@gmail.com>
2020-09-23 23:03:59 +0900

    백준 - 탐색 알고리즘(기본) - 새 (진행중)

--- algorithm-java/src/boj/searchAlgorithm/basic/bird/Main.java
+++ algorithm-java/src/boj/searchAlgorithm/basic/bird/Main.java
@@ -4,6 +4,7 @@ package boj.searchAlgorithm.basic.bird;
  *   문제명 : 새
  *
  *   - https://www.acmicpc.net/problem/1568
+ *   - 성공
  * */
 
 import java.util.Scanner;
@@ -11,5 +12,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int birdCnt = sc.nextInt();
+        int n = 0;
+        int time = 0;
+        while (birdCnt > 0) {
+            n++;
+            if (n <= birdCnt) {
+                time++;
+                birdCnt -= n;
+            } else {
+                n = 0;
+            }
+        }
+        System.out.println(time);
     }
 }
