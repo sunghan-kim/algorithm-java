ba7293a855cffa57173d1ee9afd04c99e6aa5ac8
shkim4738 <shkim4738@gmail.com>
2020-09-24 21:15:40 +0900

    백준 - 탐색 알고리즘(기본) - 베스트셀러 (진행중)

---
+++ algorithm-java/src/boj/searchAlgorithm/basic/bestseller/Main.java
@@ -0,0 +1,25 @@
+package boj.searchAlgorithm.basic.bestseller;
+
+/*
+ *   문제명 : 베스트셀러
+ *
+ *   - https://www.acmicpc.net/problem/1302
+ * */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        br.close();
+    }
+}
