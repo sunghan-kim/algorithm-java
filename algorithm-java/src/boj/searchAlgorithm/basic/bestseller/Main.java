cea61474d9135b3eb4e8b310e0a73a063f14829f
shkim4738 <shkim4738@gmail.com>
2020-09-29 22:31:39 +0900

    백준 - 탐색 알고리즘(기본) - 베스트셀러

--- algorithm-java/src/boj/searchAlgorithm/basic/bestseller/Main.java
+++ algorithm-java/src/boj/searchAlgorithm/basic/bestseller/Main.java
@@ -4,23 +4,45 @@ package boj.searchAlgorithm.basic.bestseller;
  *   문제명 : 베스트셀러
  *
  *   - https://www.acmicpc.net/problem/1302
+ *   - 성공
  * */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<String, Integer> books = new HashMap<>();
+
         for (int i=0; i < n; i++) {
-            String book = st.nextToken();
+            String book = br.readLine();
+            
+            /* - 아래 로직으로 대체 가능
+            if (books.get(book) == null) {
+                books.put(book, 1);
+            } else {
+                books.put(book, books.get(book)+1);
+            }
+            */
+            books.merge(book, 1, Integer::sum);
         }
-        HashMap<String, Integer> map = new HashMap<>();
+
+        int maxCount = Collections.max(books.values());
+
+        List<String> target = new ArrayList<>();
+        for (String key : books.keySet()) {
+            if (books.get(key) == maxCount) {
+                target.add(key);
+            }
+        }
+
+        Collections.sort(target);
+
+        System.out.println(target.get(0));
 
         br.close();
     }
