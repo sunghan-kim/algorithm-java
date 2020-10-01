f66fea549a00a9b3ba6b73ba820141731c8a0f07
shkim4738 <shkim4738@gmail.com>
2020-10-01 22:20:05 +0900

    백준 - 탐색 알고리즘(기본) - 성 지키기 (진행중)

---
+++ algorithm-java/src/boj/searchAlgorithm/basic/defendingCastle/Main.java
@@ -0,0 +1,26 @@
+package boj.searchAlgorithm.basic.defendingCastle;
+/*
+ *   문제명 : 성 지키기
+ *
+ *   - https://www.acmicpc.net/problem/1236
+ * */
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+
+
+        br.close();
+    }
+}
