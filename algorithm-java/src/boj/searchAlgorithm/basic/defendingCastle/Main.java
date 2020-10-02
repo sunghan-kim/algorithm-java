7110fc32fa36807f98dc2fcd84c4bbaa8cce4c8c
shkim4738 <shkim4738@gmail.com>
2020-10-02 20:03:34 +0900

    백준 - 탐색 알고리즘(기본) - 성 지키기

--- algorithm-java/src/boj/searchAlgorithm/basic/defendingCastle/Main.java
+++ algorithm-java/src/boj/searchAlgorithm/basic/defendingCastle/Main.java
@@ -3,6 +3,7 @@ package boj.searchAlgorithm.basic.defendingCastle;
  *   문제명 : 성 지키기
  *
  *   - https://www.acmicpc.net/problem/1236
+ *   - 성공
  * */
 
 
@@ -19,7 +20,35 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
+        String[] status = new String[n];
 
+        for (int i=0; i < n; i++) {
+            status[i] = br.readLine();
+        }
+
+        int[] row = new int[n];
+        int[] col = new int[m];
+
+        for (int i=0; i < n; i++) {
+            for (int j=0; j < m; j++) {
+                if (status[i].charAt(j) == 'X') {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        int rowCnt = 0;
+        for (int r : row) {
+            if (r == 0) rowCnt++;
+        }
+
+        int colCnt = 0;
+        for (int c : col) {
+            if (c == 0) colCnt++;
+        }
+
+        System.out.println(Math.max(rowCnt, colCnt));
 
         br.close();
     }
