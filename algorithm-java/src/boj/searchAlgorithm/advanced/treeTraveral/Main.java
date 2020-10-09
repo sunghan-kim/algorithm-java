e380128a46278f78993f3d9d9e8bb3c181c6c990
shkim4738 <shkim4738@gmail.com>
2020-10-09 22:16:05 +0900

    백준 - 탐색 알고리즘(기본) - 중량 제한

--- algorithm-java/src/boj/searchAlgorithm/advanced/treeTraveral/Main.java
+++ algorithm-java/src/boj/searchAlgorithm/advanced/treeTraveral/Main.java
@@ -7,5 +7,32 @@ package boj.searchAlgorithm.advanced.treeTraveral;
  *
  * */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static class Node {
+        String data;
+        Node leftNode;
+        Node rightNode;
+
+        public Node(String data, Node leftNode, Node rightNode) {
+            this.data = data;
+            this.leftNode = leftNode;
+            this.rightNode = rightNode;
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+
+        br.close();
+    }
 }
