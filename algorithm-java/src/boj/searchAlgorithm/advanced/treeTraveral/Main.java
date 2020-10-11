a14f26bb5a9be6cccef2c682a13f66ebfe1d30e9
shkim4738 <shkim4738@gmail.com>
2020-10-11 21:40:20 +0900

    백준 - 탐색 알고리즘(고급) - 트리 순회 (진행중)

--- algorithm-java/src/boj/searchAlgorithm/advanced/treeTraveral/Main.java
+++ algorithm-java/src/boj/searchAlgorithm/advanced/treeTraveral/Main.java
@@ -10,20 +10,33 @@ package boj.searchAlgorithm.advanced.treeTraveral;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    public static Map<String, Node> tree;
+
     public static class Node {
         String data;
-        Node leftNode;
-        Node rightNode;
+        String leftNode;
+        String rightNode;
 
-        public Node(String data, Node leftNode, Node rightNode) {
+        public Node(String data, String leftNode, String rightNode) {
             this.data = data;
             this.leftNode = leftNode;
             this.rightNode = rightNode;
         }
+    }
 
-
+    public static void preOrder(Node node) {
+        System.out.print(node.data);
+        if (node.leftNode != ".") {
+            preOrder(tree.get(node.leftNode));
+        }
+        if (node.rightNode != ".") {
+            preOrder(tree.get(node.rightNode));
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -31,6 +44,21 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
+        for (int i=0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String data = st.nextToken();
+            String leftNode = st.nextToken();
+            String rightNode = st.nextToken();
+
+            System.out.println(data + " : " + leftNode + " : " + rightNode);
+
+            tree.put(data, new Node(data, leftNode, rightNode));
+        }
+
+        System.out.println(tree);
+
+        preOrder(tree.get("A"));
+        System.out.println();
 
 
         br.close();
